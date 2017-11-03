5941a7e6beb258ab654974435a3b27141394f5b9
Christian Kuhl <git@ckuhl.com>
2017-11-03 10:19:52 -0400

    [refactor] Introduce logging

--- TODO.md
+++ TODO.md
@@ -1,6 +1,8 @@
 # TODO
-* change gene.run into a generator
-	* terminate as soon as you get the required output (optimize)
-* make the generation running multi-threaded / async
-	* each gene.run is _not_ dependant on any other
+- change gene.run into a generator
+	- terminate as soon as you get the required output (optimize)
+- make the generation running multi-threaded / async
+	- each gene.run is _not_ dependant on any other
+- create a custom logging formatter to colourize "WARN", etc.
+- fix broken tests (...)
 
--- gp/__main__.py
+++ gp/__main__.py
@@ -1,16 +1,17 @@
-from termcolor import cprint
+import logging
 
 import evolve
 import trainer
 
+
 if __name__ == '__main__':
-    print('Starting...')
+    logging.basicConfig(level=logging.DEBUG)
+    log = logging.getLogger(__name__)
+    log.info('Starting...')
     try:
         simulation = evolve.Evolve(trainer.Hello, per_gen=16)
         simulation.generate_solution()
     except KeyboardInterrupt:
-        # NOTE: The '\b' are to delete the ^C that is placed by pressing CTRL + C
         if __debug__:
-            cprint('\b\b[WARN]', 'yellow', sep='', end='\t')
-            print('Keyboard interrupt received')
+            log.warn('Keyboard interrupt received')
 
